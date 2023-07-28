84708ac72f3765e46e66fb11c215ba710c8b6062
Nghia1608 <95606486+Nghia1608@users.noreply.github.com>
2023-07-28 15:02:37 +0700

    Update JwtAuthenticationFilter.java

--- src/main/java/com/phuquy/config/JwtAuthenticationFilter.java
+++ src/main/java/com/phuquy/config/JwtAuthenticationFilter.java
@@ -38,10 +38,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        if (request.getServletPath().contains("/auth")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
         try {
             String username;
             Cookie[] cookies = request.getCookies();
